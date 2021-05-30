f2c77da52e0752cb63f64e605ac4162ad9e4b4e9
Rodrigo <rodrigomartins@id.uff.br>
2021-05-30 18:12:48 -0300

    first commit

--- app.py
+++ app.py
@@ -1,50 +1,37 @@
 import streamlit as st
-from tika import parser
-from gtts import lang
-from gtts import gTTS
-import os
-import base64
+import sympy as sym
+st.title("Equation Solver")
+@st.cache(allow_output_mutation=True)
+def variables():
+	return []
 
-def main():
-	st.title("Document to Speech Assistant")
+menu = ["Derivative", "Integration"]
+choice = st.sidebar.selectbox("Operation",menu)
 
-	initials = lang.tts_langs()
-	languages = {v: k for k, v in initials.items()}	
-	menu = sorted(list(languages.keys()))
-	choice = st.sidebar.selectbox("Languages",menu)
-	selected_language = languages[choice]
+text_input = st.text_input(label='Enter parameters')
+if st.button("Submit parameters"):
+	variables().append(text_input)
+	variables_list = [x for x in variables() if x]
+	st.success(variables_list)
 
-	st.subheader("Upload file")
-	document = st.file_uploader(" ",type=['txt','docx','pdf'])
-	if st.button("Process"):
-		if document is not None:
-			file_details = {"Filename":document.name,"FileType":document.type,"FileSize":document.size}
-			st.write(file_details)
-			try:
-				status = st.empty()
-
-				load_text = "Processing text"
-				status.markdown("%s" % load_text)
-				raw = parser.from_file(document)
-				data = raw['content']
-
-				load_text = "Processing audio"
-				status.markdown("%s" % load_text)
-				tts = gTTS(text = data, lang = selected_language)
-				filename = document.name.split('.')[0]+'.mp3'
-				tts.save(filename)
-
-				load_text = "Processing download"
-				status.markdown("%s" % load_text)
-				with open(filename, 'rb') as f:
-					mp3rb = f.read()				    
-				b64 = base64.b64encode(mp3rb).decode()
-				href = f'<a href="data:application/octet-stream;base64,{b64}" download="{os.path.basename(filename)}">Download {filename}</a>'
-				st.markdown(href, unsafe_allow_html=True)
-						
-			except Exception as e:
-				print(e)
-				if str(e).startswith('429'): st.write("This application uses Google API, then its today's free limit has been reached.")
-				
-if __name__ == '__main__':
-	main()
+if st.button("Clear parameters"):
+	variables().clear()
+	variables_list = [x for x in variables() if x]
+	st.success('No parameters')
+ 
+if len(variables())>0:
+	variables_list = [x for x in variables() if x]
+	selected_variables = st.multiselect(label= 'Enter variable:', options= variables_list, default=[])
+	
+	function_string = st.text_input(label='Enter function')
+	dict_variables = {variable:sym.symbols(variable) for variable in variables_list}
+	
+	for variable in variables_list:
+		function_string.replace(variable, "dict_variables['"+variable+"']")
+	if function_string!= '': st.latex(sym.sympify(function_string))
+	if st.button("Submit function"):
+		function_operated = sym.sympify(function_string)
+		for selected_variable in selected_variables:
+			if choice == "Derivative": function_operated = sym.diff(function_operated,dict_variables[selected_variable])
+			if choice == "Integration": function_operated = sym.integrate(function_symbolic,dict_variables[selected_variable])
+		st.latex(function_operated)	
